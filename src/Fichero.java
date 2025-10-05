0a81f6b41dd0ad6b84125a9bbf5bbc9ea8356ecc
Carlos <carloscano654@gmail.com>
2025-10-05 20:21:13 +0200

    Correccion

--- src/Fichero.java
+++ src/Fichero.java
@@ -3,24 +3,22 @@ import java.util.Date;
 
 public class Fichero {
     public static void main(String[] args) {
-            File archivo = new File("/Users/carloscanocastrillon/");
+            File archivo = new File("/Users/carloscanocastrillon/Desktop/Escritorio - MacBook Pro de Carlos/DAM");
+            //Ejercio 1
             File [] ficheros = archivo.listFiles();
             Date diaMs = new Date();
 
-
             for (File f: ficheros) {
                 if (f.isFile()) {
-                    System.out.println(f.getName() + " es un fichero");
-                    //Tamaño del archivo
-                    System.out.println(f.length() + " bytes");
-                    //Fecha de la ultima modificacion
-                    diaMs.setTime(f.lastModified());
-                    System.out.println(diaMs+ " fecha de la ultima modificación");
+                    System.out.println("[ARCHIVO] " +f.getName() + " " + f.length() + " bytes " + " - Última modificación " + f.lastModified());
+                    
                 }
 
                 if (f.isDirectory()) {
-                    System.out.println(f.getName() + " es un directorio");
+                    System.out.println("[DIRECTORIO] " +f.getName() + " - Última modificación " + f.lastModified());
+
                 }
             }
+
     }
 }
